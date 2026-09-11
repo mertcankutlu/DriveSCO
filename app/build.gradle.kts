540dcc7fb3d2230548f674842e3671da4209a916
mertcankutlu <mertck04@gmail.com>
2026-09-11 21:34:57 +0300

    Set DriveSCO version to Beta V1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mertcankutlu.drivesco"
         minSdk = 29
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "Beta V1.0.1"
     }
 
     compileOptions {
